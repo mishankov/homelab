f78aadbcab3f86112a8c4176c283180845a8c61e
Denis Mishankov <mishankov@main.com>
2025-04-05 08:42:41 +0300

    add resolv script

---
+++ scripts/resolv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Проверяем, существует ли файл resolv.conf
+if [ ! -f "/etc/resolv.conf" ]; then
+    echo "Файл /etc/resolv.conf не найден"
+    exit 1
+fi
+
+# Проверяем права на запись
+if [ ! -w "/etc/resolv.conf" ]; then
+    echo "Нет прав на запись в файл /etc/resolv.conf"
+    exit 1
+fi
+
+# Проверяем наличие строки с 8.8.8.8
+if grep -q "8.8.8.8" "/etc/resolv.conf"; then
+    echo "DNS сервер 8.8.8.8 уже настроен"
+else
+    # Добавляем новые строки в конец файла
+    echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+    echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+    echo "DNS серверы 8.8.8.8 и 8.8.4.4 успешно добавлены"
+fi
+
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
